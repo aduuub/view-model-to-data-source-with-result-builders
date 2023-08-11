7911268d441ac3b8ee29dc16011ece12796aea81
Adam Wareing <wareing.adam@abc.net.au>
2023-08-11 17:08:33 +1000

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Adam Wareing on 11/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
